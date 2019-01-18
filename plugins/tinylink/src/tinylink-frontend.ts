b105f69666ee32f013410571e2e47f8388e40c2c
daixinye <denight@qq.com>
2019-01-18 23:42:32 +0800

    open file picker by clicking webview content

--- plugins/tinylink/src/tinylink-frontend.ts
+++ plugins/tinylink/src/tinylink-frontend.ts
@@ -5,12 +5,12 @@
 import * as theia from "@theia/plugin";
 
 namespace IoTCommands {
-    const IOT_CATEGORY = 'IoT Plugin';
+    const IOT_CATEGORY = "IoT Plugin";
 
     export const TINYLINK_COMPILE = {
-        id: 'iot.plugin.tinylink.compile',
+        id: "iot.plugin.tinylink.compile",
         category: IOT_CATEGORY,
-        label: 'TinyLink Compile',
+        label: "TinyLink Compile"
     };
 
     export const TINYSIM_COMPILE = {
@@ -22,8 +22,8 @@ namespace IoTCommands {
     export const ONELINK_COMPILE = {
         id: "iot.plugin.onelink.compile",
         category: IOT_CATEGORY,
-        label: 'OneLink Compile'
-    }
+        label: "OneLink Compile"
+    };
 }
 
 export function start(context: theia.PluginContext) {
@@ -31,28 +31,56 @@ export function start(context: theia.PluginContext) {
         theia.commands.registerCommand(
             IoTCommands.TINYLINK_COMPILE,
             async (...args: any[]) => {
-                theia.window
-                    .showOpenDialog({
-                        defaultUri: theia.Uri.parse("/home/project")
-                    })
-                    .then(val => {
-                        if (!val || !val.length) {
-                            return theia.window.showErrorMessage('没有选择文件')
-                        }
+                const panel = theia.window.createWebviewPanel(
+                    "TinyLink Compile Result",
+                    "TinyLink Compile Result",
+                    theia.ViewColumn.Active
+                );
+                panel.webview.html = `
+                        <script>
+                            window.addEventListener("message",function(e){
+                                var data = e.data.data
+                                var from = e.data.from
+                                switch(from){
+                                    case 'webide':
+                                        document.getElementById('iframe').contentWindow.postMessage({
+                                            from: 'webview',
+                                            data: data
+                                        },'*')
+                                        break
+                                    case 'iframe':
+                                        window.postMessageExt(data)
+                                        break
+                                }
+                            },false)
+                        </script>
+                        <iframe id="iframe" src="http://tinylink.daixinye.com/webview/debug" frameborder="0" style="display: block; margin: 0px; overflow: hidden; position: absolute; width: 100%; height: 100%; visibility: visible;" sandbox="allow-same-origin allow-scripts"></iframe>
+                        `;
+                panel.webview.onDidReceiveMessage((data: any) => {
+                    if (data && data.type === "command") {
+                        switch (data.content) {
+                            case "open_file_picker":
+                                theia.window
+                                    .showOpenDialog({
+                                        defaultUri: theia.Uri.parse("/home/project")
+                                    })
+                                    .then((val: any) => {
+                                        if (!val || !val.length) {
+                                            return theia.window.showErrorMessage("没有选择文件");
+                                        }
+                                        panel.webview.postMessage({
+                                            from: 'webide',
+                                            data: {
+                                                path: val[0].path
+                                            }
+                                        })
 
-                        let result = (val && val[0]) || { path: "" };
-                        const panel = theia.window.createWebviewPanel(
-                            "TinyLink Compile Result",
-                            "TinyLink Compile Result",
-                            theia.ViewColumn.Active
-                        );
-                        let params = theia.env.getQueryParameters() || {};
-                        let queryString = `path=${result.path}`;
-                        for (let key in params) {
-                            queryString += `&${key}=${params[key]}`;
+                                    });
+                                break;
+                            default:
                         }
-                        panel.webview.html = `<iframe src="http://tinylink.daixinye.com/webview/tinylink?${queryString}" frameborder="0" style="display: block; margin: 0px; overflow: hidden; position: absolute; width: 100%; height: 100%; visibility: visible;" sandbox="allow-same-origin allow-scripts"></iframe>`;
-                    });
+                    }
+                });
             }
         )
     );
